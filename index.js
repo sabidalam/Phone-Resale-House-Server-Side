369f59bab7f5df1ed3a6e1801a3a2498715cb1f7
sabidalam <Shafkatsaad@gmail.com>
2022-11-24 23:24:39 +0600

    conneted-mongoDb

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ifta3d6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const categoriesCollection = client.db('phoneResaleHouse').collection('brandsCategories');
+        const allPhoneCollection = client.db('phoneResaleHouse').collection('allPhones');
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { id: id };
+            const result = await allPhoneCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
 
 
 app.get('/', (req, res) => {
