5d058c8a1291d97f4a73fbb0f3a332f26ea79661
sabidalam <Shafkatsaad@gmail.com>
2022-11-25 19:29:57 +0600

    added-booking-collection

--- index.js
+++ index.js
@@ -15,17 +15,26 @@ async function run() {
     try {
         const categoriesCollection = client.db('phoneResaleHouse').collection('brandsCategories');
         const allPhoneCollection = client.db('phoneResaleHouse').collection('allPhones');
+        const bookingsCollection = client.db('phoneResaleHouse').collection('bookings');
 
+        //categories
         app.get('/categories', async (req, res) => {
             const query = {};
             const result = await categoriesCollection.find(query).toArray();
             res.send(result);
-        })
+        });
 
-        app.get('/category/:id', async (req, res) => {
+        app.get('/categories/:id', async (req, res) => {
             const id = req.params.id
             const query = { id: id };
             const result = await allPhoneCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        //bookings
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
     }
